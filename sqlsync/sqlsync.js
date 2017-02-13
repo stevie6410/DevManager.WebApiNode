dee35961c812c53c41e4e6e7d211182d7b4e5ad2
Steve Kent <steve_kent@beaerospace.com>
2017-02-13 16:49:28 +0000

    Removed comments

--- sqlsync/sqlsync.js
+++ sqlsync/sqlsync.js
@@ -1,44 +1,5 @@
 "use strict"
 
-// var Connection = require('tedious').Connection;
-// var config = {
-//     server: 'DC0348',
-//     userName: 'appUser',
-//     password: 'Password2',
-//     options: {
-//         database: 'ReportData'
-//     }
-// };
-// var connection = new Connection(config);
-// var Request = require('tedious').Request;
-
-// connection.on('connect', function (err) {
-//     if (!err) {
-//         console.log('Connected to db');
-//         executeStatment();
-//     } else {
-//         console.log('Connnection failed: ', err);
-//     }
-// });
-
-// function executeStatment() {
-//     var request = new Request("SELECT TOP 2 * FROM sys.objects", function (err, rowCount) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(rowCount + ' rows');
-//         }
-//     });
-
-//     request.on('row', function (columns) {
-//         columns.forEach(function (column) {
-//             console.log(column.value);
-//         });
-//     });
-
-//     connection.execSql(request);
-// }
-
 var Request = require('tedious').Request;
 
 module.exports = class {
